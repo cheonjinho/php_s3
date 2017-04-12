0ec4c891edf74436aed4626d7557e0f9a94d5a50
jin <sr_soou@naver.com>
2017-04-12 10:48:01 +0900

    getting started s3

--- index.php
+++ index.php
@@ -313,3 +313,6 @@ switch (ENVIRONMENT)
  * And away we go...
  */
 require_once BASEPATH.'core/CodeIgniter.php';
+require 'vendor/autoload.php';
+require 'application/aws/aws.phar';
+require 'application/aws/aws-autoloader.php';
