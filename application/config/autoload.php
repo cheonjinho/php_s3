355c9b628051f8903858ba62a34f98de0f02eedd
jin <sr_soou@naver.com>
2017-04-13 14:49:38 +0900

    test helper

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('aws');
+$autoload['helper'] = array('aws', 'private');
 
 /*
 | -------------------------------------------------------------------
